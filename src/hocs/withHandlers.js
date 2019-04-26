99ca1beba932d8c29121cc18beace5ce34495d4e
Romain Pellerin <rpellerin@users.noreply.github.com>
2019-04-26 16:27:38 +0200

    Oopsie

--- src/hocs/withHandlers.js
+++ src/hocs/withHandlers.js
@@ -1,3 +1,9 @@
 import React from 'react'
 
-export default addedHandlers => Component => props => null // ?
+export default addedFunctions => Component => props => {
+  const addedProps = Object.entries(addedFunctions).reduce(
+    (acc, [key, v]) => ({ ...acc, [key]: v(props) }),
+    {}
+  )
+  return <Component {...addedProps} {...props} />
+}
